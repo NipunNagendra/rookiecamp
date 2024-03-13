84c5fbcaf42f35d425b6964d9d8f975446589e79
nipun <nipun.nagendra9@gmail.com>
2024-03-12 23:13:37 -0500

    added CustomMecanumKinematics and testing class for power using forward kinematics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/CustomMecanumKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/CustomMecanumKinematics.java
@@ -3,32 +3,27 @@ package org.firstinspires.ftc.teamcode.libs;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.TwoWheelTrackingLocalizer;
+
 import java.util.Arrays;
 import java.util.List;
 
 
 public class CustomMecanumKinematics {
     static Movement driveBase;
-
+    static SampleMecanumDrive driveRR;
+    static TwoWheelTrackingLocalizer localizer;
     public static double lateralMultiplier = 1.0;
     public static double trackWidth = 1.0;
     public static double wheelBase = 1.0;
+    public static Pose2d givenPowerPose;
     public static double VX_WEIGHT = 1.0;
     public static double VY_WEIGHT = 1.0;
     public static double OMEGA_WEIGHT = 1.0;
 
-
-    public CustomMecanumKinematics(HardwareMap hardwareMap) {
-        driveBase = new Movement(hardwareMap);
-    }
-
-    public void setDrivePower(Pose2d drivePower, double powerProportion) {
-
-    }
-
-    public double[] setWeightedDrivePower(Pose2d drivePower) {
+    public static Pose2d normalizedDrivePowerPose(Pose2d drivePower) {
         Pose2d vel = drivePower;
-
         if (Math.abs(drivePower.getX()) + Math.abs(drivePower.getY()) + Math.abs(drivePower.getHeading()) > 1) {
             double denom = VX_WEIGHT * Math.abs(drivePower.getX()) + VY_WEIGHT * Math.abs(drivePower.getY()) + OMEGA_WEIGHT * Math.abs(drivePower.getHeading());
             vel = new Pose2d(
@@ -38,11 +33,11 @@ public class CustomMecanumKinematics {
             ).div(denom);
         }
 
-        return inverseKinematicModel(drivePower, trackWidth, wheelBase, lateralMultiplier);
+        return vel;
+
     }
 
-
-    public static double[] inverseKinematicModel(Pose2d robotVel, double trackWidth, double wheelBase, double lateralMultiplier) {
+    public static double[] inverseKinematicModel(Pose2d robotVel) {
         double d = (trackWidth + wheelBase) / 2.0;
         return new double[]{
                 robotVel.getX() - lateralMultiplier * robotVel.getY() - d * robotVel.getHeading(),
@@ -52,15 +47,17 @@ public class CustomMecanumKinematics {
         };
     }
 
-    public static Pose2d forwardKinematicModel(List<Double> wheelVelocities, double trackWidth, double wheelBase, double lateralMultiplier) {
+    public static Pose2d forwardKinematicModel(List<Double> wheelVelocities) {
         double d = (trackWidth + wheelBase) / 2.0;
         return new Pose2d(
                 (wheelVelocities.get(0) + wheelVelocities.get(1) + wheelVelocities.get(2) + wheelVelocities.get(3)) / 4.0,
-                (wheelVelocities.get(0) - wheelVelocities.get(1) - wheelVelocities.get(2) + wheelVelocities.get(3)) / 4.0 * lateralMultiplier,
+                (wheelVelocities.get(0) - wheelVelocities.get(1) - wheelVelocities.get(2) + wheelVelocities.get(3)) / (4.0 * lateralMultiplier),
                 (wheelVelocities.get(0) - wheelVelocities.get(1) + wheelVelocities.get(2) - wheelVelocities.get(3)) / (4.0 * d)
         );
 
     }
+
+
 
 }
 
